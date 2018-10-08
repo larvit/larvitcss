b3b44040fe484f3866dc244da7079ecda48060c8
Klas Espenkrona <klas@larvit.se>
2018-10-08 13:56:04 +0200

    Updated controller to work with version 0.5.X of larvitbase. Upped dependencies and tests

--- controllers/css.js
+++ controllers/css.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const	autoprefixer = require('autoprefixer'),
-	compiledCss  = {},
-	postcss      = require('postcss'),
-	path         = require('path'),
-	sass         = require('node-sass'),
-	Lfs          = require('larvitfs'),
-	lfs          = new Lfs(),
-	log          = require('winston');
+const autoprefixer = require('autoprefixer');
+const compiledCss  = {};
+const postcss      = require('postcss');
+const path         = require('path');
+const sass         = require('node-sass');
+const Lfs          = require('larvitfs');
+const lfs          = new Lfs();
 
 function serveCss(compiled, req, res) {
 	res.setHeader('Last-Modified', compiled.lastModified);
@@ -16,23 +15,25 @@ function serveCss(compiled, req, res) {
 }
 
 function autoprefix(compiled, req, res) {
-	postcss([autoprefixer]).process(compiled.str).then(function(result) {
-		result.warnings().forEach(function(warn) {
-			log.warn('larvitcss: controllers/css.js: autoprefix() - Warning from postcss: ' + warn.toString());
+	postcss([autoprefixer]).process(compiled.str)
+		.then(function (result) {
+			result.warnings().forEach(function (warn) {
+				req.log.warn('larvitcss: controllers/css.js: autoprefix() - Warning from postcss: ' + warn.toString());
+			});
+			compiled.str = result.css;
+			serveCss(compiled, req, res);
 		});
-		compiled.str = result.css;
-		serveCss(compiled, req, res);
-	});
 }
 
-exports.run = function(req, res, cb) {
-	let srcPath,
-	    parsed;
+module.exports = function (req, res, cb) {
+	let srcPath;
+	let parsed;
 
 	// Serve cached version
 	if (compiledCss[req.urlParsed.pathname] !== undefined && process.env.NODE_ENV !== 'development') {
-		log.debug('larvitcss: controllers/css.js: "' + req.urlParsed.pathname + ' found in cache, serving directly!');
+		req.log.debug('larvitcss: controllers/css.js: "' + req.urlParsed.pathname + ' found in cache, serving directly!');
 		autoprefix(compiledCss[req.urlParsed.pathname], req, res);
+
 		return;
 	}
 
@@ -60,16 +61,16 @@ exports.run = function(req, res, cb) {
 		return;
 	}
 
-	log.debug('larvitcss: controllers/css.js: resolved "' + req.urlParsed.pathname + '" to "' + srcPath + '", compile!');
+	req.log.debug('larvitcss: controllers/css.js: resolved "' + req.urlParsed.pathname + '" to "' + srcPath + '", compile!');
 
-	sass.render({'file': srcPath, 'outputStyle': 'compressed'}, function(err, result) {
+	sass.render({'file': srcPath, 'outputStyle': 'compressed'}, function (err, result) {
 		if (err) {
-			log.warn('larvitcss: controllers/css.js: Could not render ' + srcPath + ' err: ' + err.message);
+			req.log.warn('larvitcss: controllers/css.js: Could not render ' + srcPath + ' err: ' + err.message);
 			result = {'css': new Buffer([])};
 		}
 
 		compiledCss[req.urlParsed.pathname] = {
-			'str':          result.css.toString(),
+			'str': result.css.toString(),
 			'lastModified': new Date()
 		};
 		autoprefix(compiledCss[req.urlParsed.pathname], req, res);
--- test/test.js
+++ test/test.js
@@ -1,79 +1,79 @@
 'use strict';
 
-const request = require('request'),
-      assert  = require('assert'),
-      log     = require('winston');
+const request   = require('request');
+const lUtils    = new (require('larvitutils'))();
+const test  = require('tape');
+const App   = require('larvitbase-www');
+const log = new lUtils.Log('warning');
 
+let app;
 let port;
-
-// Set up winston
-log.remove(log.transports.Console);
-log.add(log.transports.Console, {
-	'level':     'warn',
-	'colorize':  true,
-	'timestamp': true,
-	'json':      false
-});
 
 process.cwd('..');
 
-before(function(done) {
-	require('freeport')(function(err, tmpPort) {
-		assert( ! err, 'err should be negative');
+test('Start webserver', function (t) {
+	require('freeport')(function (err, tmpPort) {
+		if (err) throw err;
 
 		port = tmpPort;
 
-		// Start the server up
-		require('larvitbase')({
-			'port': port,
-			'customRoutes': [{
-				'regex': '\\.css$',
-				'controllerName': 'css'
-			}]
+		app	= new App({
+			'baseOptions': { 'httpOptions':	tmpPort},
+			'log':	log,
+			'routerOptions': {
+				'routes': [{
+					'regex': '\\.css$',
+					'controllerPath': 'css.js'
+				}]
+			}
+		});
+		app.middleware.splice(3, 0, function (req, res, cb) {
+			req.log = log;
+			cb();
 		});
 
-		done();
+		app.start(t.end);
 	});
 });
 
-describe('Basics', function() {
-	it('Test a basic, single SCSS file', function(done) {
-		request('http://localhost:' + port + '/test/foo.css', function(err, res, body) {
-			assert( ! err, 'err must be negative');
-			assert.deepEqual(res.statusCode, 200);
-			assert.deepEqual(res.headers['content-type'], 'text/css');
-			assert.deepEqual(body, 'body{font:100% Helvetica,sans-serif;color:#333}\n');
-			done();
-		});
-	});
-
-	it('Get the source for that SCSS file', function(done) {
-		request('http://localhost:' + port + '/test/foo.scss', function(err, res, body) {
-			assert( ! err, 'err must be negative');
-			assert.deepEqual(res.statusCode, 200);
-			assert.deepEqual(res.headers['content-type'], 'text/x-scss; charset=UTF-8');
-			assert.deepEqual(body, '$font-stack:    Helvetica, sans-serif;\n$primary-color: #333;\n\nbody {\n  font: 100% $font-stack;\n  color: $primary-color;\n}');
-			done();
-		});
-	});
-
-	it('Get nested SCSS files', function(done) {
-		request('http://localhost:' + port + '/test/nested.css', function(err, res, body) {
-			assert( ! err, 'err must be negative');
-			assert.deepEqual(res.statusCode, 200);
-			assert.deepEqual(res.headers['content-type'], 'text/css');
-			assert.deepEqual(body, 'a{color:#f00}body{background:#0f0}\n');
-			done();
-		});
+test('Test a basic, single SCSS file', function (t) {
+	request('http://localhost:' + port + '/test/foo.css', function (err, res, body) {
+		if (err) throw err;
+		t.equal(res.statusCode, 200);
+		t.deepEqual(res.headers['content-type'], 'text/css');
+		t.deepEqual(body, 'body{font:100% Helvetica,sans-serif;color:#333}\n');
+		t.end();
 	});
 });
 
-describe('Special rules', function() {
-	it('Get uncompiled pre-css before compiled css', function(done) {
-		request('http://localhost:' + port + '/test/blubb.css', function(err, res, body) {
-			assert( ! err, 'err must be negative');
-			assert.deepEqual(body, 'a{font-decoration:none}\n');
-			done();
-		});
+test('Get the source for that SCSS file', function (t) {
+	request('http://localhost:' + port + '/test/foo.scss', function (err, res, body) {
+		if (err) throw err;
+		t.deepEqual(res.statusCode, 200);
+		t.deepEqual(res.headers['content-type'], 'text/x-scss; charset=UTF-8');
+		t.deepEqual(body, '$font-stack:    Helvetica, sans-serif;\n$primary-color: #333;\n\nbody {\n  font: 100% $font-stack;\n  color: $primary-color;\n}');
+		t.end();
 	});
+});
+
+test('Get nested SCSS files', function (t) {
+	request('http://localhost:' + port + '/test/nested.css', function (err, res, body) {
+		if (err) throw err;
+		t.deepEqual(res.statusCode, 200);
+		t.deepEqual(res.headers['content-type'], 'text/css');
+		t.deepEqual(body, 'a{color:#f00}body{background:#0f0}\n');
+		t.end();
+	});
+});
+
+test('Get uncompiled pre-css before compiled css', function (t) {
+	request('http://localhost:' + port + '/test/blubb.css', function (err, res, body) {
+		if (err) throw err;
+		t.deepEqual(body, 'a{font-decoration:none}\n');
+		t.end();
+	});
+});
+
+test('Stop webserver', function (t) {
+	app.stop(t.end);
 });
